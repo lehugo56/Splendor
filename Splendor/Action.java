77dcaba8b73ffa24053db689d78d6ffa91a5f47d
lehugo56 <96341046+lehugo56@users.noreply.github.com>
2024-12-06 12:58:33 +0100

    Add files via upload

---
+++ Splendor/Action.java
@@ -0,0 +1,13 @@
+
+/**
+ * Décrivez votre interface Action ici.
+ *
+ * @author  (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+
+public interface Action
+{
+    public void process(String action);
+    public String toString();
+}
