30070e2c3d9f3d85675e37dbbd6f9a3471899aa0
King-of-Baka <genoux.m06@gmail.com>
2024-12-10 14:23:57 +0100

    Update Player.java

--- Splendor/Player.java
+++ Splendor/Player.java
@@ -1,9 +1,31 @@
-
+import java.util.ArrayList;
 public abstract class Player implements Displayable {
 
-    /* --- Stringers --- */
-   
-     
+    private int id;
+    private String name;
+    private int prestige;
+    private ArrayList<DevCard> purchaseCards;
+    private Resources resources;
+    
+    public  Player(int id, String name){
+        this.id=id;
+        this.name=name;
+        prestige=0;
+        purchaseCards=new ArrayList<DevCard>();
+        resources=new resources();
+    }
+    public String getnom(){
+        return nom;
+    }
+    public int getpoints(){
+        return prestige;
+    }
+    public  int getNbTokens(){
+        
+    }
+    public  int getAvailableResources(){
+        getAvailableResources();
+    }
     public String[] toStringArray(){
         /** EXAMPLE. The number of resource tokens is shown in brackets (), and the number of cards purchased from that resource in square brackets [].
          * Player 1: Camille
@@ -17,8 +39,7 @@ public abstract class Player implements Displayable {
          */
         String pointStr = " ";
         String[] strPlayer = new String[8];
-         /*
-            * A decommenter une fois la classe implémentée
+        
         if(points>0){
             pointStr = new String(new int[] {getPoints()+9311}, 0, 1);
         }else{
@@ -32,7 +53,7 @@ public abstract class Player implements Displayable {
         for(ACOMPLETER){ //-- parcourir l'ensemble des resources (res) en utilisant l'énumération Resource
             strPlayer[3+(Resource.values().length-1-res.ordinal())] = res.toSymbol() + " ("+resources.getNbResource(res)+") ["+getResFromCards(res)+"]";
         }
-        */
+        
         return strPlayer;
     }
 }
