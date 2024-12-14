226ccc965886547ddcd5a5c214bd6be266bb75f7
Mxn0n <mm56.jeux@gmail.com>
2024-12-14 20:29:35 +0100

    Ajout de commentaires + modif de setNbResource

--- Splendor/Resources.java
+++ Splendor/Resources.java
@@ -1,4 +1,3 @@
-
 /**
  * Décrivez votre classe Resources ici.
  *
@@ -10,6 +9,12 @@ public class Resources
 {
     Resource re;
     private int[] listeResources;
+    
+    /**
+     * Constructeur de la classe Resources
+     * 
+     * tableau d'entiers de la forme [tier, diamond, sapphire, emerald, ruby, onyx, point, type]
+     */
     public Resources() {
         // Initialisation du tableau avec 8 éléments à 0
         
@@ -17,39 +22,66 @@ public class Resources
             listeResources = new int[8];
         }
     }
+    
+    /**
+     * Acesseur du nombre de ressources disponible pour un type donné
+     * 
+     * @param le type de la ressource (Resource)
+     * @return le nombre (int)
+     */
     public int getNbResource(Resource elements_resources){
         switch(elements_resources){
-            case DIAMOND:
+            case Resource.DIAMOND:
                 return listeResources[1];
-            case SAPPHIRE:
+            case Resource.SAPPHIRE:
                 return listeResources[2];
-            case EMERALD:
+            case Resource.EMERALD:
                 return listeResources[3];
-            case RUBY:
+            case Resource.RUBY:
                 return listeResources[4];
-            case ONYX:
+            case Resource.ONYX:
                 return listeResources[5];
             default:
                 return 0;
         }
         
     }
+    
+    /**
+     * Manipulateur du nombre de ressources pour un type donné
+     * 
+     * @param le type de ressource dont on veut faire la modification (Resource)
+     * @param la nouvelle valeur de la ressource (int)
+     *
+     */
     public void setNbResource(Resource elements_resources,int new_resources){
-        switch(elements_resources){
-            case DIAMOND:
-                listeResources[1]=new_resources;
-            case SAPPHIRE:
-                listeResources[2]=new_resources;
-            case EMERALD:
-                listeResources[3]=new_resources;
-            case RUBY:
-                listeResources[4]=new_resources;
-            case ONYX:
-                listeResources[5]=new_resources;
+        if(elements_resources.equals(Resource.DIAMOND)){
+            listeResources[1]=new_resources;  
+            }
+        else if (elements_resources.equals(Resource.SAPPHIRE)){
+            listeResources[2]=new_resources;
+        }
+        else if (elements_resources.equals(Resource.EMERALD)){
+            listeResources[3]=new_resources;
+        }
+        else if (elements_resources.equals(Resource.RUBY)){
+            listeResources[4]=new_resources;
+        }
+        else if (elements_resources.equals(Resource.ONYX)){
+            listeResources[5]=new_resources;
         }   
     }
-
     
+    /**
+     * Met à jour le nombre de ressource (pour un type donné) en fonction d'une quantité
+     * si quantité > 0, ajoute
+     * si quantité < 0, supprime
+     * 
+     * @param le type de ressource (Resource)
+     * @param la quantité que l'on veut modifier (int)
+     * 
+     * @return le nombre de ressource actualisé
+     */
     public void updateNbResource(Resource elements_resources, int quantité){
         int somme=getNbResource(elements_resources)+quantité;
         if(somme>0){
@@ -58,9 +90,14 @@ public class Resources
         else{
             setNbResource(elements_resources,0);
         }
-        
     }
     
+    
+    /**
+     * Permet de connaitre la disponibilité des ressources
+     * 
+     * @return les types de ressources (indice) pour lesquels des ressources sont disponibles sous forme de liste
+     */
     public int[] getAvailableResources(){
         
         int cpt=-1;
@@ -70,7 +107,7 @@ public class Resources
                 cpt+=1;
             }
         }
-        if (cpt==-1){ //verifie que la liste n'est pas nul
+        if (cpt==-1){ //verifie que la liste n'est pas nulle
             return null;
         }
         else{
